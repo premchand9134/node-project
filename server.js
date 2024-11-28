584361738f3eb998ae4fd2b3109b0b71700c2038
learningGithub <premchand9134@gmail.com>
2024-11-28 13:51:11 +0530

    fixing

--- server.js
+++ server.js
@@ -32,16 +32,6 @@ db.on('error',(error)=>{
 })
 
 app.get('/',(req,res)=>{
-    // const {token} = req.cookies;
-    // if(token){
-    //     const tokenData = jwt.verify(token,process.env.JWT_SECRET_KEY)
-    //     if(tokenData.type == 'user'){
-    //         res.render('home')
-    //     }
-    // }else{
-    //     res.redirect('/signin')
-    // }
-    
     const {token} = req.cookies;
     if(token){
         const tokenData = jwt.verify(token,process.env.JWT_SECRET_KEY)
@@ -51,7 +41,7 @@ app.get('/',(req,res)=>{
     }else{
         res.redirect('/signin')
     }
-    
+      
     
 
     
@@ -66,74 +56,44 @@ app.get('/signup',(req,res)=>{
 })
 
 app.post('/signup', async (req,res)=>{
-    // const {name,email,password:plainTextPassword} = req.body;
-    // const salt = await bcrypt.genSalt(10)
-    // const encryptedPassword = await bcrypt.hashSync(plainTextPassword,salt)
-
-    // try {        
-    //     await user.create({
-    //         name,
-    //         email,
-    //         password:encryptedPassword
-    //     });
-    //     res.redirect('/signin')
-
-    // } catch (error) {
-    //     console.log(error);        
-    // }
-
     const {name,email,password:plainTextPassword} = req.body;
     const salt = await bcrypt.genSalt(10)
-    const encryptedPassword =await bcrypt.hashSync(plainTextPassword,salt)
+    const encryptedPassword = await bcrypt.hashSync(plainTextPassword,salt)
 
-    try {
+    try {        
         await user.create({
             name,
             email,
             password:encryptedPassword
         });
         res.redirect('/signin')
+
     } catch (error) {
-        console.log(error);
-        
+        console.log(error);        
     }
+
+    
 
 
     
 })
 
 app.post('/signin',async(req,res)=>{
-    // const {email,password} = req.body;
+    const {email,password} = req.body;
 
-    // const userObj = await user.findOne({email})
-    // if(!userObj){
-    //     res.send({error:"User not found",status : 404})
-    // }
-
-    // if(await bcrypt.compare(password,userObj.password)){
-    //     const token = jwt.sign({
-    //         userId : userObj._id,email : email, type : 'user' 
-    //     },process.env.JWT_SECRET_KEY,{expiresIn:'2h'})
-    //     res.cookie('token',token,{maxAge:2*60*60*1000});
-    //     res.redirect('/')       
-        
-    // }
-
-
-    const {email,password} =req.body;
-
-    const userObj = await user.findOne({email});
+    const userObj = await user.findOne({email})
     if(!userObj){
-        res.send({error:"User not found"})
+        res.send({error:"User not found",status : 404})
     }
+
     if(await bcrypt.compare(password,userObj.password)){
         const token = jwt.sign({
-            userId : userObj._id,email:email,type:"user"
-        },process.env.JWT_SECRET_KEY,{expiresIn:"2h"})
-        res.cookie('token',token,{maxAge:2*60*60*1000})
-        res.redirect('/')
-    }
-    
+            userId : userObj._id,email : email, type : 'user' 
+        },process.env.JWT_SECRET_KEY,{expiresIn:'2h'})
+        res.cookie('token',token,{maxAge:2*60*60*1000});
+        res.redirect('/')       
+        
+    }    
 
     
     
